20577942b53dffe35bc2eb4471e9c8a055a71d60
Fernando Sanchez <fersanchezone@outlook.com>
2017-05-02 15:11:47 -0700

    pendiente

--- Baja/Baja.Web/Models/ManageViewModels.cs
+++ Baja/Baja.Web/Models/ManageViewModels.cs
@@ -12,6 +12,9 @@ namespace Baja.Web.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+
+
+ 
     }
 
     public class ManageLoginsViewModel
