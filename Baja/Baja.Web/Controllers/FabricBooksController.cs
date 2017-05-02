81681b6f3b8cca50ca336a81039714f83febe24e
Marcelino <Marcelino@DESKTOP-T8J4U4V>
2017-05-02 10:22:18 -0700

    Fabric

---
+++ Baja/Baja.Web/Controllers/FabricBooksController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Baja.Domain;
+using Baja.Domain.Fabric;
+
+namespace Baja.Web.Controllers
+{
+    public class FabricBooksController : Controller
+    {
+        private BajaDbContext db = new BajaDbContext();
+
+        // GET: FabricBooks
+        public ActionResult Index()
+        {
+            var fabricBooks = db.FabricBooks.Include(f => f.FabricCategories);
+            return View(fabricBooks.ToList());
+        }
+
+        // GET: FabricBooks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricBook fabricBook = db.FabricBooks.Find(id);
+            if (fabricBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricBook);
+        }
+
+        // GET: FabricBooks/Create
+        public ActionResult Create()
+        {
+            ViewBag.FabricCategoryId = new SelectList(db.FabricCategories, "Id", "Name");
+            return View();
+        }
+
+        // POST: FabricBooks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,FabricCategoryId")] FabricBook fabricBook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FabricBooks.Add(fabricBook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FabricCategoryId = new SelectList(db.FabricCategories, "Id", "Name", fabricBook.FabricCategoryId);
+            return View(fabricBook);
+        }
+
+        // GET: FabricBooks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricBook fabricBook = db.FabricBooks.Find(id);
+            if (fabricBook == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FabricCategoryId = new SelectList(db.FabricCategories, "Id", "Name", fabricBook.FabricCategoryId);
+            return View(fabricBook);
+        }
+
+        // POST: FabricBooks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,FabricCategoryId")] FabricBook fabricBook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fabricBook).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FabricCategoryId = new SelectList(db.FabricCategories, "Id", "Name", fabricBook.FabricCategoryId);
+            return View(fabricBook);
+        }
+
+        // GET: FabricBooks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricBook fabricBook = db.FabricBooks.Find(id);
+            if (fabricBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricBook);
+        }
+
+        // POST: FabricBooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FabricBook fabricBook = db.FabricBooks.Find(id);
+            db.FabricBooks.Remove(fabricBook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Baja/Baja.Web/Controllers/FabricCategoriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Baja.Domain;
+using Baja.Domain.Fabric;
+
+namespace Baja.Web.Controllers
+{
+    public class FabricCategoriesController : Controller
+    {
+        private BajaDbContext db = new BajaDbContext();
+
+        // GET: FabricCategories
+        public ActionResult Index()
+        {
+            return View(db.FabricCategories.ToList());
+        }
+
+        // GET: FabricCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricCategory fabricCategory = db.FabricCategories.Find(id);
+            if (fabricCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricCategory);
+        }
+
+        // GET: FabricCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FabricCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] FabricCategory fabricCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FabricCategories.Add(fabricCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fabricCategory);
+        }
+
+        // GET: FabricCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricCategory fabricCategory = db.FabricCategories.Find(id);
+            if (fabricCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricCategory);
+        }
+
+        // POST: FabricCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] FabricCategory fabricCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fabricCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricCategory);
+        }
+
+        // GET: FabricCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FabricCategory fabricCategory = db.FabricCategories.Find(id);
+            if (fabricCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricCategory);
+        }
+
+        // POST: FabricCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FabricCategory fabricCategory = db.FabricCategories.Find(id);
+            db.FabricCategories.Remove(fabricCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
