c71f9b6d22defc777e3b9b76b24b86e1e69577ec
Marcelino <Marcelino@DESKTOP-T8J4U4V>
2017-05-04 22:04:54 -0700

    RoleCreateAndIndex

---
+++ Baja/Baja.Web/Controllers/RoleController.cs
@@ -0,0 +1,82 @@
+﻿using Baja.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baja.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+            return View(roles);
+        }
+       
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            db.Roles.Add(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            IdentityRole role = db.Roles.Find(id);
+            db.Roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //// GET: Role/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Role/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+
+        //    return View();
+
+        //}
+
+
+    }
+}
