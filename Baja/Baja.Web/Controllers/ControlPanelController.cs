6968670cd338137d85ac500f61c70a76221b5686
Fernando Sanchez Bravo <fersanchezonecesun@outlook.com>
2017-05-05 14:36:07 -0700

    Unir bootstrap y materialize

---
+++ Baja/Baja.Web/Controllers/ControlPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baja.Web.Controllers
+{
+    public class ControlPanelController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
