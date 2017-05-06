6c7d12a410304ccbf9033f96a267f180e91ff3db
Fernando Sanchez Bravo <fersanchezonecesun@outlook.com>
2017-05-05 17:58:05 -0700

    Implementacion de Ajax en Fabrics Categories Crud completo + combinacion de materialize  y boostrap

--- Baja/Baja.Web/Controllers/FabricCategoriesController.cs
+++ Baja/Baja.Web/Controllers/FabricCategoriesController.cs
@@ -18,103 +18,77 @@ namespace Baja.Web.Controllers
         // GET: FabricCategories
         public ActionResult Index()
         {
-            return View(db.FabricCategories.ToList());
-        }
-
-        // GET: FabricCategories/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FabricCategory fabricCategory = db.FabricCategories.Find(id);
-            if (fabricCategory == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fabricCategory);
-        }
-
-        // GET: FabricCategories/Create
-        public ActionResult Create()
-        {
             return View();
         }
 
+        public ActionResult GetCategories()
+        {
+            var tblcategories = db.FabricCategories.ToList();
+            return Json(tblcategories, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Get(int id)
+        {
+            var categorie = db.FabricCategories.ToList().Find(x => x.Id == id);
+            return Json(categorie, JsonRequestBehavior.AllowGet);
+        }
+
+
+        //// GET: FabricCategories/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    FabricCategory fabricCategory = db.FabricCategories.Find(id);
+        //    if (fabricCategory == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(fabricCategory);
+        //}
+
+
         // POST: FabricCategories/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name")] FabricCategory fabricCategory)
+        public ActionResult Create([Bind(Exclude = "Id")] FabricCategory fabricCategory)
         {
             if (ModelState.IsValid)
             {
                 db.FabricCategories.Add(fabricCategory);
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
 
-            return View(fabricCategory);
+            return Json(fabricCategory, JsonRequestBehavior.AllowGet);
         }
 
-        // GET: FabricCategories/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FabricCategory fabricCategory = db.FabricCategories.Find(id);
-            if (fabricCategory == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fabricCategory);
-        }
-
-        // POST: FabricCategories/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name")] FabricCategory fabricCategory)
+        public ActionResult Update(FabricCategory fabricCategory)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(fabricCategory).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
-            return View(fabricCategory);
+
+            return Json(fabricCategory, JsonRequestBehavior.AllowGet);
         }
 
         // GET: FabricCategories/Delete/5
-        public ActionResult Delete(int? id)
+        [HttpPost]
+        public ActionResult Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FabricCategory fabricCategory = db.FabricCategories.Find(id);
-            if (fabricCategory == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fabricCategory);
-        }
+            var fabricCategorie = db.FabricCategories.ToList().Find(x => x.Id == id);
 
-        // POST: FabricCategories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            FabricCategory fabricCategory = db.FabricCategories.Find(id);
-            db.FabricCategories.Remove(fabricCategory);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            if (fabricCategorie != null)
+            {
+                db.FabricCategories.Remove(fabricCategorie);
+                db.SaveChanges();
+            }
+            return Json(fabricCategorie, JsonRequestBehavior.AllowGet);
         }
+    
 
         protected override void Dispose(bool disposing)
         {
