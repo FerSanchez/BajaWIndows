6a6235e58a3edeafcf1b7a5e15d2d722254b71fd
Marcelino <Marcelino@DESKTOP-T8J4U4V>
2017-05-08 16:33:52 -0700

    UserController Ready

---
+++ Baja/Baja.Domain/User/UserRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baja.Domain.User
+{
+    public class UserRoleViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+
+        public string RoleName { get; set; }
+    }
+}
